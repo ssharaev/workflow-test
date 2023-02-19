06d3648b4b74737a0a037cda22f97af471c50150
svyatoslav <slawizkema@ya.ru>
2023-02-19 17:41:54 +0400

    add h2 database

--- src/test/java/ru/ssharaev/AppTest.java
+++ src/test/java/ru/ssharaev/AppTest.java
@@ -2,10 +2,12 @@ package ru.ssharaev;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes = {App.class, H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
 class AppTest {
 
     @Test
