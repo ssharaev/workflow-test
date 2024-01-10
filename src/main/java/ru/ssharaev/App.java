d0cc37273629ff4f84fade82aeaeeae264f32400
Sviatoslav Sharaev <sviatoslav.sharaev@gmail.com>
2024-01-10 22:36:03 +0400

    release fix test

--- src/main/java/ru/ssharaev/App.java
+++ src/main/java/ru/ssharaev/App.java
@@ -10,7 +10,7 @@ public class App {
     private static final int TYPE_MASK = ARRAY_MASK | CONST_MASK;
     public static void main(String[] args) {
         //SpringApplication.run(App.class, args);
-        System.out.println(0.3 == 3.0 /10);
+        //System.out.println(0.3 == 3.0 /10);
         System.out.println(Integer.toString(TYPE_MASK, 2));
     }
 }
