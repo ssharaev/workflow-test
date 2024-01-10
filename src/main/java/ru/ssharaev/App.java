42309f023a83c075681ab2d8f7decbaa0cb2330f
Sviatoslav Sharaev <sviatoslav.sharaev@gmail.com>
2024-01-10 22:17:33 +0400

    release fix

--- src/main/java/ru/ssharaev/App.java
+++ src/main/java/ru/ssharaev/App.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    private static final int ARRAY_MASK = 1 << 31;
+    private static final int CONST_MASK = 1 << 30;
+    private static final int TYPE_MASK = ARRAY_MASK | CONST_MASK;
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        //SpringApplication.run(App.class, args);
+        System.out.println(0.3 == 3.0 /10);
+        System.out.println(Integer.toString(TYPE_MASK, 2));
     }
 }
